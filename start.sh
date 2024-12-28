94696b251622a5e8a6668e05edcb4a959503b09c
Luis Villaseñor <lvillasen@gmail.com>
2024-12-28 11:38:48 -0600

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,28 @@
+# ...28/Dec/24
+# ...Author: LV
+# ...make the bash scripts executable with
+# chmod a+x start.sh
+# chmod a+x stop.sh
+# ...Check  crontab is installed with
+# systemctl status cron
+# ...Enter 
+# crontab -l
+# ...Check this line shows up
+# */5 * * * * /usr/bin/python3 /root/Muon-Flux/muonFluxCrontab.py > /var/log/cron.log 2>&1
+# ...If it does not insert it with 
+# crontab -e
+# ...To see the log file enter
+# grep CRON /var/log/syslog
+# ...or
+# cat /var/log/cron.log
+# ...or
+# journalctl -u cron
+# ...Si no hay log file install it with
+# sudo apt update
+# sudo apt install rsyslog
+# systemctl restart rsyslog
+
+rm /root/Muon-Flux/Rates.json
+systemctl start cron
+echo "Muon Flux DAQ started ....."
+date
---
+++ stop.sh
@@ -0,0 +1,6 @@
+# ...28/Dec/24
+# ...Author: LV
+# ...Detener crontab con
+systemctl stop cron
+echo "Muon Flux DAQ stopped ....."
+date
